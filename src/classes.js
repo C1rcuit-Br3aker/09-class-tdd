9992271919d091466899cc0ca90125bf3396e5e0
Chris Boone <cboone1337@gmail.com>
2016-03-15 14:42:20 -0500

    Foo and dog working

--- src/classes.js
+++ src/classes.js
@@ -3,4 +3,20 @@ class Foo {
 
 }
 
-var bar;
+var bar = new Foo;
+
+class Dog {
+  constructor(says) {
+    this.says = `life is ruff`;
+  }
+
+}
+
+class Cat {
+  constructor(growl) {
+    this.growl = 'growls';
+  }
+}
+
+var garfield = new Cat(garfield);
+garfield.growl;
